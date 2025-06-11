3fc1f58efb9b7ce0e9cc0a0c8025998fb8864ed1
Muhammad Hasan Kamal <128207930+ikmalsworld@users.noreply.github.com>
2025-06-11 21:23:34 +0700

    reversion to pre prometheus grafana

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\MetricsController;
 
 Route::get('/', [StockController::class, 'index'])->name('home');
 Route::post('/stocks', [StockController::class, 'store'])->name('stocks.store');
 Route::post('/stocks/adjust/{id}', [StockController::class, 'adjust'])->name('stocks.adjust');
 Route::delete('/stocks/{id}', [StockController::class, 'destroy'])->name('stocks.destroy');
 Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-Route::get('/metrics', [MetricsController::class, 'metrics']);
+
 
